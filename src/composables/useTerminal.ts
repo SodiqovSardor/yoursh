20e261472bebd66dc8e0dacc43a09f489767c7dc
yoursh <you@yoursh.local>
2026-08-19 07:49:58 +0500

    Term container uses 100vw/100vh directly + delayed re-fit

--- src/composables/useTerminal.ts
+++ src/composables/useTerminal.ts
@@ -14,6 +14,7 @@ export function useTerminal(container: HTMLElement, config: SshConfig) {
   term.open(container)
   fit.fit()
   requestAnimationFrame(() => fit.fit())
+  setTimeout(() => fit.fit(), 100)
 
   const ws = new WebSocket(WS_URL)
   ws.binaryType = 'arraybuffer'
